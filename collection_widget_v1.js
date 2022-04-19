d336915235d5f5368e7c7bb1530c5ae8b753d73e
RareGems <86891370+raregems-io@users.noreply.github.com>
2022-04-19 20:29:31 +0300

    Update collection_widget_v1.js
    latest version

--- collection_widget_v1.js
+++ collection_widget_v1.js
@@ -14,20 +14,20 @@
         <style>
             #__raregems_collection_widget {
                 all: unset; box-sizing: border-box; padding: 12px; border: 2px solid ${dark ? '#212529' : '#ced4da'}; border-radius: 8px; background: ${dark ? '#131316' : '#f8f9fa'}; color: ${dark ? '#f8f9fa' : '#212529'};
-                font-family: system-ui; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; text-transform: uppercase; cursor: pointer;
+                font-family: system-ui; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; line-height: 1; text-transform: uppercase; cursor: pointer;
             }
             #__raregems_collection_widget:hover { border-color: #228be6; }
             #__raregems_collection_widget .__rg_header { font-size: 10px; font-weight: bold; letter-spacing: 1px; color: ${dark ? '#868e96' : '#868e96'}; text-align: center; }
             #__raregems_collection_widget .__rg_header img { display: block; margin: -1px auto 0; }
             #__raregems_collection_widget .__rg_items { display: flex; gap: 8px; margin-top: 8px; }
-            #__raregems_collection_widget .__rg_item { flex: 1; padding: 8px; text-align: center; background: ${dark ? '#212529' : '#dee2e6'}; border-radius: 8px; }
+            #__raregems_collection_widget .__rg_item { flex: 1; padding: 10px; text-align: center; background: ${dark ? '#212529' : '#dee2e6'}; border-radius: 8px; }
             #__raregems_collection_widget .__rg_name { font-size: 10px; text-transform: uppercase; color: ${dark ? '#adb5bd' : '#495057'}; }
-            #__raregems_collection_widget .__rg_value { margin-top: 4px; font-size: 16px; font-weight: bold; }
-            #__raregems_collection_widget .__rg_value img { display: inline-block; vertical-align: -2px; }
+            #__raregems_collection_widget .__rg_value { margin-top: 8px; font-size: 16px; font-weight: bold; }
+            #__raregems_collection_widget .__rg_value img { display: inline-block; margin-right: 4px; vertical-align: -2px; }
         </style>
         <div class="__rg_header">
             Tradable on
-            <img src="https://s.raregems.io/img/logo${dark ? '_white' : '_black'}.svg" width="100" height="21">
+            <img src="https://s.raregems.io/img/logo${dark ? '_white' : '_black'}.svg" width="100" height="21" alt="RareGems">
         </div>
         <div class="__rg_items">
             <div class="__rg_item">
@@ -52,9 +52,12 @@
     const r = await (await fetch(`https://raregems.io/api/collection_widget?chain=${chain}&collection=${collection}`)).json()
 
     const items = root.querySelectorAll('.__rg_item')
-    items[0].lastElementChild.textContent = r.tokens_count
-    items[1].lastElementChild.textContent = r.owners_count
-    items[2].lastElementChild.textContent = r.offers_count
-    items[3].lastElementChild.innerHTML = `<img src="https://s.raregems.io/img/chains/${chain}.svg" width="16" height="16"> ${r.floor_price}`
+    items[0].lastElementChild.textContent = Number(r.tokens_count)
+    items[1].lastElementChild.textContent = Number(r.owners_count)
+    items[2].lastElementChild.textContent = Number(r.offers_count)
+
+    items[3].lastElementChild.innerHTML = `<img src="https://s.raregems.io/img/chains/${chain}.svg" width="16" height="16">`
+    items[3].lastElementChild.append(Number(r.floor_price))
+
 
 })()
